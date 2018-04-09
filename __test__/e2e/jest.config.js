8e30f967018673fe29f742858365ea8b50a7d11d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-09 15:15:05 -0300

    Move e2e tests

--- __test__/e2e/jest.config.js
+++
@@ -1,7 +0,0 @@
-const config = require('../../jest.config.js')
-
-module.exports = {
-  ...config,
-  testMatch: [`${__dirname}/**/*.spec.js`],
-  setupTestFrameworkScriptFile: `${__dirname}/setup.js`
-}

8ed064ea9a59c3248e906ac4db1e431e7dfe077a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-23 15:28:09 -0300

    Add listings search header component

--- containers/views/Listings/Search/Form.js
+++ containers/views/Listings/Search/Form.js
@@ -1,12 +1,23 @@
 import {Component} from 'react'
+import {NavigationActions} from 'react-navigation'
 
 import Shell from '@/containers/shared/Shell'
 import Form from '@/components/listings/Search/Form'
+import Header from '@/components/listings/Search/Header'
 
 export default class SearchFormScreen extends Component {
   onChange = (params) => {
     const {navigation} = this.props
     navigation.setParams(params)
+  }
+
+  onReset = () => this.onChange({})
+
+  onReturn = () => {
+    const {navigation} = this.props
+    const {parent, ...params} = navigation.state.params
+    navigation.dispatch(NavigationActions.setParams({params, key: parent}))
+    navigation.goBack(null)
   }
 
   onPressNeighborhoods = () => {
@@ -19,7 +30,16 @@ export default class SearchFormScreen extends Component {
     const {params} = this.props.navigation.state
 
     return (
-      <Shell scroll>
+      <Shell
+        scroll
+        header={
+          <Header
+            title="Filtrar busca"
+            onReturn={this.onReturn}
+            onReset={this.onReset}
+          />
+        }
+      >
         <Form
           onChange={this.onChange}
           onPressNeighborhoods={this.onPressNeighborhoods}

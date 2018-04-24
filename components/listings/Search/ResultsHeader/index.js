81c48f1f6fe56c3494307dfbf978dcbe61e0e642
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-24 15:41:52 -0300

    Fix filter button empty condition

--- components/listings/Search/ResultsHeader/index.js
+++ components/listings/Search/ResultsHeader/index.js
@@ -1,9 +1,11 @@
-import _ from 'lodash'
+import _ from 'lodash/fp'
 import {View, TouchableOpacity} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import Text from '@/components/shared/Text'
 import styles from './styles'
+
+const isEmpty = _.flow(_.pickBy(_.identity), _.isEmpty)
 
 export default function ResultsHeader({onPress, value}) {
   return (
@@ -11,7 +13,7 @@ export default function ResultsHeader({onPress, value}) {
       <View style={styles.container}>
         <Icon style={styles.icon} name="filter-outline" />
         <Text style={[styles.text, value && styles.textActive]}>
-          {!_.isEmpty(value) ? 'Filtros aplicados' : 'Sem filtros'}
+          {!isEmpty(value) ? 'Filtros aplicados' : 'Sem filtros'}
         </Text>
         <Text style={styles.button}>Filtrar</Text>
       </View>

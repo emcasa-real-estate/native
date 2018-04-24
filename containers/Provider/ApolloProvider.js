ee3deb8e55ca8670e76a988dfd03c429a988b730
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-24 16:52:17 -0300

    Setup apollo client

---
+++ containers/Provider/ApolloProvider.js
@@ -0,0 +1,38 @@
+import {Component} from 'react'
+import {ApolloProvider} from 'react-apollo'
+import {connect} from 'react-redux'
+
+import createClient from '@/lib/apollo'
+import {getToken} from '@/redux/modules/auth/selectors'
+
+class AppApolloProvider extends Component {
+  state = {
+    client: null
+  }
+
+  constructor(props) {
+    super(props)
+    this.state.client = createClient({request: this.request})
+  }
+
+  request = (operation) => {
+    const {jwt} = this.props
+    operation.setContext({
+      headers: {
+        authorization: jwt ? `Token ${jwt}` : undefined
+      }
+    })
+  }
+
+  render() {
+    const {children} = this.props
+    const {client} = this.state
+    return <ApolloProvider client={client}>{children}</ApolloProvider>
+  }
+}
+
+const props = (state) => ({
+  jwt: getToken(state)
+})
+
+export default connect(props)(AppApolloProvider)
--- containers/Provider/index.js
+++ containers/Provider/index.js
@@ -3,13 +3,16 @@ import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react'
 
 import {store, persistor} from '@/redux'
+import ApolloProvider from './ApolloProvider'
 
 export default class AppProvider extends Component {
   render() {
     const {children} = this.props
     return (
       <Provider store={store}>
-        <PersistGate persistor={persistor}>{children}</PersistGate>
+        <PersistGate persistor={persistor}>
+          <ApolloProvider>{children}</ApolloProvider>
+        </PersistGate>
       </Provider>
     )
   }

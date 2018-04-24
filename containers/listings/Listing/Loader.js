8fa9d70ad3d078c043dd799f326234e625ff48d9
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-24 19:56:34 -0300

    Add tour view mutation to listing container

--- containers/listings/Listing/Loader.js
+++ containers/listings/Listing/Loader.js
@@ -1,15 +1,28 @@
 import _ from 'lodash'
 import {PureComponent} from 'react'
-import {connect} from 'react-redux'
+import {connect, compose} from 'react-redux'
+import {graphql} from 'react-apollo'
 
+import {VISUALIZE_TOUR} from '@/lib/mutations/listings'
 import {load} from '@/redux/modules/listings/data'
 import {getData, isLoading} from '@/redux/modules/listings/data/selectors'
 import Loader from '@/containers/shared/Loader'
 
 export class ListingLoader extends PureComponent {
+  state = {
+    visualizedTour: false
+  }
+
   onLoad = () => {
     const {load, id} = this.props
     load(id)
+  }
+
+  onViewTour = () => {
+    if (!this.state.visualizedTour) {
+      this.setState({visualizedTour: true})
+      this.props.onViewTour()
+    }
   }
 
   get status() {
@@ -38,4 +51,13 @@ const actions = {
 
 export const withListing = connect(props, actions)
 
-export default withListing(ListingLoader)
+export const withMutations = graphql(VISUALIZE_TOUR, {
+  options: ({id}) => ({
+    variables: {id}
+  }),
+  props: ({mutate}) => ({
+    onViewTour: () => mutate()
+  })
+})
+
+export default compose(withListing, withMutations)(ListingLoader)

044a5bdd28eedd71586c9b85d192f22bef48694a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-25 12:57:47 -0300

    Fix tests

--- __test__/setup.js
+++ __test__/setup.js
@@ -3,6 +3,8 @@ import path from 'path'
 
 const MOCKS_DIR = path.resolve(__dirname, '../__mocks__')
 
+jest.mock('react-dom/server', () => {}, {virtual: true})
+
 // Setup node_modules mocks manually
 fs.readdirSync(MOCKS_DIR).map((filename) => {
   jest.mock(filename.replace(/\.[^/.]+$/, ''))

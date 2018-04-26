4edcb541243845ac4522484fc6d45030fa5ff120
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-26 17:31:08 -0300

    Header styles

--- components/listings/Search/ResultsHeader/styles.js
+++ components/listings/Search/ResultsHeader/styles.js
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native'
 
 import * as colors from '@/assets/colors'
-import {elevation, margin} from '@/assets/styles'
+import {margin, padding} from '@/assets/styles'
 
 export default StyleSheet.create({
   container: {
@@ -10,12 +10,7 @@ export default StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: 'white',
-    borderColor: colors.gray.lighter,
-    borderWidth: 0.8,
-    borderRadius: 5,
-    padding: 10,
-    ...margin(10, 20),
-    ...elevation(6)
+    ...padding(10, 20, 15)
   },
   text: {
     flex: 1,

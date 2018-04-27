d3c30683dc2bad004fc06365b98d1c75e0699b5f
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-27 18:53:06 -0300

    Fix beta apk name

--- .buildkite/android/deploy.sh
+++ .buildkite/android/deploy.sh
@@ -2,7 +2,7 @@ set -e
 
 case $BUILD_PROFILE in
   beta)
-    bundle exec fastlane android beta bin:$ROOT/android/app/build/outputs/apk/app-release.apk groups:"$TESTER_GROUPS"
+    bundle exec fastlane android beta bin:$ROOT/android/app/build/outputs/apk/app-staging.apk groups:"$TESTER_GROUPS"
     ;;
   production)
     cd android && ./gradlew publishApkRelease

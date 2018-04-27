d1233a20828307ac42649bedca856105acd84e43
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-27 13:29:11 -0300

    Fix version names

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -2,5 +2,5 @@ if [[ $BUILDKITE_BRANCH == "master" ]];
 then export TESTER_GROUPS=$BETA_TESTERS;
 else export TESTER_GROUPS=$ALPHA_TESTERS; fi
 
-if [[ $PROFILE == "production" ]];
-then export VERSION_NAME=${VERSION_NAME_PRODUCTION:-$VERSION_NAME}; fi
+if [[ $PROFILE == "production" && $VERSION_TAG ]];
+then export VERSION_NAME="$VERSION_NAME-$VERSION_TAG"; fi

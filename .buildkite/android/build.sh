9ff13bda88a426190caca7f4896e6dcce2a447cd
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-27 14:48:37 -0300

    Add android staging configuration

--- .buildkite/android/build.sh
+++ .buildkite/android/build.sh
@@ -4,8 +4,11 @@ case $BUILD_PROFILE in
   debug)
     cd android && ./gradlew assembleDebug
     ;;
-  beta|production)
-    bundle exec fastlane android build signed:true
+  beta)
+    cd android && ./gradlew assembleStaging
+    ;;
+  production)
+    cd android && ./gradlew assembleRelease
     ;;
 esac
 

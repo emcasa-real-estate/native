81e5a16e7d9e6fa7edea8c893593ac5008e5aaae
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-27 15:29:34 -0300

    Fix bump script

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,6 +1,3 @@
 if [[ $BUILDKITE_BRANCH == "master" ]];
 then export TESTER_GROUPS=$BETA_TESTERS;
 else export TESTER_GROUPS=$ALPHA_TESTERS; fi
-
-if [[ $PROFILE == "production" && $VERSION_TAG ]];
-then export VERSION_NAME="$VERSION_NAME-$VERSION_TAG"; fi

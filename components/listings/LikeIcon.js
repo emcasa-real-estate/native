7c116f1049fe641cfaefced14bc4d84c7dc25419
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-30 14:43:16 -0300

    Update listing icons

--- components/listings/LikeIcon.js
+++ components/listings/LikeIcon.js
@@ -1,18 +1,24 @@
 import * as colors from '@/assets/colors'
 import Icon from '@/components/shared/Icon'
 
-export default function LikeIcon({active, contrast}) {
+export default function LikeIcon({active, contrast, size, ...props}) {
   const color = contrast ? 'black' : 'rgba(0,0,0,0)'
   const border = contrast ? 'white' : 'black'
   return (
     <Icon
       name="heart"
       type="solid"
+      size={size - 2}
       stroke={border}
-      strokeWidth={25}
+      strokeWidth={size}
       strokeLinecap="round"
       color={active ? colors.red.medium : color}
       fillOpacity={active ? 1 : 0.2}
+      {...props}
     />
   )
+}
+
+LikeIcon.defaultProps = {
+  size: 25
 }

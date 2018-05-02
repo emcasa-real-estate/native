b5ba84b04decb5d4336a1db730d68f7311c63414
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-02 17:20:38 -0300

    Fix react native bundle on android

--- .buildkite/android/build.sh
+++ .buildkite/android/build.sh
@@ -2,6 +2,10 @@ set -e
 
 echo "Building apk for $BUILD_PROFILE"
 
+react-native bundle --platform android --dev false --entry-file index.js \
+  --bundle-output android/app/src/main/assets/index.android.bundle \
+  --assets-dest android/app/src/main/res/
+
 case $BUILD_PROFILE in
   debug)
     cd android && ./gradlew assembleDebug

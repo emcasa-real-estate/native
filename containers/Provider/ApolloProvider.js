300784456ad9b8e71713ef622e8b181e83235333
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-03 17:40:12 -0300

    Remove authLink

--- containers/Provider/ApolloProvider.js
+++ containers/Provider/ApolloProvider.js
@@ -2,6 +2,7 @@ import {Component} from 'react'
 import {ApolloProvider} from 'react-apollo'
 import {connect} from 'react-redux'
 
+import {GET_FAVORITE_LISTINGS_IDS} from '@/lib/graphql/queries/favorites'
 import client from '@/lib/graphql/client'
 import {getToken} from '@/redux/modules/auth/selectors'
 
@@ -9,12 +10,12 @@ class AppApolloProvider extends Component {
   componentDidUpdate(prev) {
     const {jwt} = this.props
     if (prev.jwt === jwt) return
-    if (jwt) true
+    if (jwt) this.state.sync(client)
     else client.resetStore()
   }
 
-  get authQueue() {
-    return client._links.get('auth').queue
+  get state() {
+    return client._links.get('state')
   }
 
   render() {

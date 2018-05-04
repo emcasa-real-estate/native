468a014b98081a9b0176048114df47e5d452dad5
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-04 00:53:33 -0300

    Stop redirecting to login on favorites screen

--- containers/listings/Feed/Favorites.js
+++ containers/listings/Feed/Favorites.js
@@ -1,38 +1,18 @@
 import {Component} from 'react'
-import {graphql} from 'react-apollo'
-import {connect} from 'react-redux'
 import {mapProps} from 'recompose'
 import withNavigation from 'react-navigation/src/views/withNavigation'
 
-import {getToken} from '@/redux/modules/auth/selectors'
 import Feed from '@/components/listings/Feed/Listing'
 import Card from '@/containers/listings/Card/Listing'
 import {withFavoriteListings} from '@/containers/listings/FavoritesQuery'
 
 @withNavigation
-@connect((state) => ({
-  online: state.network.isConnected,
-  jwt: getToken(state)
-}))
 @withFavoriteListings
 @mapProps(({favorites, ...props}) => ({
   ...props,
   ...favorites
 }))
 export default class FavoritesFeedApp extends Component {
-  componentDidMount() {
-    this.redirectGuests()
-  }
-
-  componentDidUpdate() {
-    this.redirectGuests()
-  }
-
-  redirectGuests() {
-    const {jwt, navigation} = this.props
-    if (!jwt) navigation.replace('auth')
-  }
-
   render() {
     const {navigation, ...props} = this.props
     return (

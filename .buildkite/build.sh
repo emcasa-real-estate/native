efd51b49e8738bcaae60e7fdec5efb042dffa107
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-06 13:50:57 -0300

    Fix buildkite api url configuration

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -16,3 +16,6 @@ cleanup() {
 trap cleanup EXIT
 
 export RCT_NO_LAUNCH_PACKAGER=TRUE
+if [[ $BUILD_PROFILE == production ]];
+then export API_URL="https://em-casa-backend.herokuapp.com";
+else export API_URL="https://em-casa-backend-staging.herokuapp.com"; fi

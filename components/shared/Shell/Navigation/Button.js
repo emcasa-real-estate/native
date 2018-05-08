b63c5fca4f10e326ca7e1bf2ca2ba99847f02b9a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-08 18:38:59 -0300

    Add active navigation style

--- components/shared/Shell/Navigation/Button.js
+++ components/shared/Shell/Navigation/Button.js
@@ -4,11 +4,17 @@ import Icon from '@/components/shared/Icon'
 import Text from '@/components/shared/Text'
 import styles, {iconColor} from './styles'
 
-export default function NavButton({children, icon, ...props}) {
+export default function NavButton({children, icon, active, ...props}) {
   return (
     <TouchableOpacity style={styles.buttonContainer} {...props}>
-      <Icon name={icon} color={iconColor} style={styles.icon} />
-      <Text style={styles.buttonText}>{children}</Text>
+      <Icon
+        name={icon}
+        color={iconColor[active ? 'active' : 'default']}
+        style={styles.icon}
+      />
+      <Text style={[styles.buttonText, active && styles.buttonTextActive]}>
+        {children}
+      </Text>
     </TouchableOpacity>
   )
 }

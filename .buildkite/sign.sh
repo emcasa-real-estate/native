7a93f850811f43d6b084286c7f8614d5b0a739b2
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-08 12:41:46 -0300

    Fix ios beta bundle id

--- .buildkite/sign.sh
+++
@@ -1,7 +0,0 @@
-if [[ $BUILD_PROFILE == production ]];
-then export CODESIGN_PROFILE=app-store;
-else export CODESIGN_PROFILE=ad-hoc; fi
-if [[ $BUILD_PROFILE == beta ]];
-then export BUNDLE_ID_SUFFIX="-beta"; fi
-
-export IOS_PROVISIONING_FILE=$(printf $IOS_PROVISIONING_FILE $CODESIGN_PROFILE)

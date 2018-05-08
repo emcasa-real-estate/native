7efe29a5ff62749ef6e4087255c0a8fc3990b034
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-08 13:16:57 -0300

    Fix beta ipa path

--- .buildkite/ios/deploy.sh
+++ .buildkite/ios/deploy.sh
@@ -1,6 +1,5 @@
 set -eu
 
-IPA_FILE="$ROOT/ios/build/EmCasa.ipa"
 PATH="$PATH:$(dirname "$(xcode-select -p)")/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/"
 export FASTLANE_USER=$APPLE_ID
 export FASTLANE_PASSWORD=$APPLE_PASSWORD
@@ -8,7 +7,7 @@ export FASTLANE_PASSWORD=$APPLE_PASSWORD
 case $BUILD_PROFILE in
   beta)
     bundle exec fastlane run crashlytics \
-      ipa_path:$IPA_FILE \
+      ipa_path:"$ROOT/ios/build/EmCasa-Beta.ipa" \
       api_token:"$FABRIC_API_KEY" \
       build_secret:"$FABRIC_BUILD_SECRET" \
       groups:"$TESTER_GROUPS"
@@ -16,7 +15,7 @@ case $BUILD_PROFILE in
   production)
     bundle exec fastlane deliver \
       --skip_metadata --skip_screenshots \
-      --ipa $IPA_FILE \
+      --ipa "$ROOT/ios/build/EmCasa.ipa" \
       --app $APPLE_APP_ID \
       --username $APPLE_ID \
       --app_version $VERSION_NAME \

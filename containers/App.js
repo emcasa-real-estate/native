fa539fdfff17cae25f2e9ba194d6efff2bb54819
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-09 20:04:13 -0300

    Fix safe area view

--- containers/App.js
+++ containers/App.js
@@ -1,5 +1,5 @@
 import {Component} from 'react'
-import {Platform, StatusBar} from 'react-native'
+import {View, StatusBar, SafeAreaView} from 'react-native'
 import {StackNavigator} from 'react-navigation'
 
 import OrientationProvider from '@/containers/shared/Orientation/Provider'
@@ -14,21 +14,20 @@ const Stack = StackNavigator(views, {
   }
 })
 
+const viewStyle = {width: '100%', height: '100%', overflow: 'hidden'}
+
 // Wrap StackNavigator in a Component class for hot module replacement
 // https://github.com/facebook/react-native/issues/8465
 export default class App extends Component {
-  componentDidMount() {
-    StatusBar.setBarStyle('dark-content')
-    // Set default statusbar styles
-    if (Platform.OS === 'android') {
-      StatusBar.setBackgroundColor('white')
-    }
-  }
-
   render() {
     return (
       <OrientationProvider>
-        <Stack />
+        <StatusBar barStyle="dark-content" backgroundColor="white" />
+        <SafeAreaView style={viewStyle}>
+          <View style={viewStyle}>
+            <Stack />
+          </View>
+        </SafeAreaView>
       </OrientationProvider>
     )
   }

f1bcfd99564889014ff2bb6a6c280bae6176e83d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-09 14:12:56 -0300

    Add layout wrapper to account screen

--- containers/views/Account.js
+++ containers/views/Account.js
@@ -1,7 +1,12 @@
+import Shell from '@/containers/shared/Shell'
 import Form from '@/components/account/EditForm'
 
 export default function AccountScreen() {
-  return <Form onSubmit={console.log} />
+  return (
+    <Shell title="Minha conta">
+      <Form onSubmit={console.log} />
+    </Shell>
+  )
 }
 
 export const screen = AccountScreen

2f713034ad5b9646d89500d6792d32c0528e7a98
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-10 18:16:40 -0300

    FormHeader component

--- components/shared/Form/SubmitHeader/index.js
+++ components/shared/Form/SubmitHeader/index.js
@@ -1,22 +1,30 @@
 import {View, TouchableOpacity} from 'react-native'
+import withNavigation from 'react-navigation/src/views/withNavigation'
 
 import Icon from '@/components/shared/Icon'
 import Text from '@/components/shared/Text'
 import Link from '@/components/shared/Link'
 import styles, {iconColor} from './styles'
 
-export default function Header({title, onReturn, onReset}) {
+function FormHeader({title, buttonText, onSubmit, onReturn, navigation}) {
+  const onReturnFun = onReturn || (() => navigation.goBack(null))
   return (
     <View style={styles.container}>
       <View style={styles.button}>
-        <TouchableOpacity onPress={onReturn}>
+        <TouchableOpacity onPress={onReturnFun}>
           <Icon name="chevron-left" size={22} color={iconColor} />
         </TouchableOpacity>
       </View>
       <Text style={styles.title}>{title}</Text>
-      <Link style={styles.resetButton} onPress={onReset}>
-        Limpar
+      <Link style={styles.resetButton} onPress={onSubmit}>
+        {buttonText}
       </Link>
     </View>
   )
 }
+
+FormHeader.defaultProps = {
+  buttonText: 'Enviar'
+}
+
+export default withNavigation(FormHeader)

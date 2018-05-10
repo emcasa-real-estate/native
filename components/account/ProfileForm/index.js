a7e57c5258c92bc21e0c2716a913423c0aadc5a8
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-10 18:20:26 -0300

    Add profile form header

--- components/account/ProfileForm/index.js
+++ components/account/ProfileForm/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash'
 import {Component} from 'react'
 import {View} from 'react-native'
+import {Gateway} from 'react-gateway'
 
 import {required} from '@/lib/validations'
 import Form from '@/components/shared/Form/Form'
 import Email from '@/components/shared/Form/Email'
 import Phone from '@/components/shared/Form/Phone'
 import TextInput from '@/components/shared/Form/TextInput'
+import Header from '@/components/shared/Form/SubmitHeader'
 import Button from '../FormButton'
 import Section from '../FormSection'
 import styles from './styles'
@@ -21,7 +23,14 @@ export default class ProfileForm extends Component {
 
     return (
       <View style={styles.container}>
-        <Form onSubmit={onSubmit} value={this.value} style={styles.form}>
+        <Gateway into="header">
+          <Header
+            title="Editar Perfil"
+            buttonText="Salvar"
+            onSubmit={onSubmit}
+          />
+        </Gateway>
+        <Form value={this.value} style={styles.form}>
           <Section title="Nome completo">
             <TextInput
               name="name"

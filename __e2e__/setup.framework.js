7ee1ca51abe49ecd12dd01e716dc42eaf8bf22a0
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-11 22:53:23 -0300

    Update dependencies

--- __e2e__/setup.framework.js
+++ __e2e__/setup.framework.js
@@ -6,13 +6,8 @@ jest.setTimeout(120000)
 
 beforeAll(async () => {
   await detox.init(pkg.detox)
-  await device.launchApp()
 })
 
 afterAll(async () => {
   await detox.cleanup()
-})
-
-beforeEach(async () => {
-  await device.reloadReactNative()
 })

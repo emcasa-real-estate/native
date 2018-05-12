6d0b6165965f37bb8df00f9aa3575bbe2a5a8943
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-12 18:15:17 -0300

    Fixup

--- scripts/ios/build.sh
+++ scripts/ios/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
+ROOT=$PWD
 cd ios && xcodebuild \
   -scheme EmCasa \
-  -archivePath $PWD/ios/build/EmCasa.xcarchive \
-  -workspace $PWD/ios/EmCasa.xcworkspace \
+  -workspace $ROOT/ios/EmCasa.xcworkspace \
   -configuration Debug \
-  -destination "generic/platform=iOS" \
   -sdk iphonesimulator \
-  -derivedDataPath $PWD/ios/build | tee $PWD/tmp/logs/ios.build.log | xcpretty
+  -derivedDataPath $ROOT/ios/build | tee $ROOT/tmp/logs/ios.build.log | xcpretty

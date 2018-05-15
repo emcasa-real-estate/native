9bcfdead2d85bead83ca8d5f5664896cf8db6f55
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-15 14:05:22 -0300

    Add address form screen

--- components/newListing/Address/styles.js
+++ components/newListing/Address/styles.js
@@ -3,15 +3,5 @@ import {StyleSheet} from 'react-native'
 export default StyleSheet.create({
   container: {
     margin: 20
-  },
-  autoComplete: {
-    height: 60,
-    overflow: 'visible',
-    zIndex: 1
-  },
-  field: {
-    overflow: 'visible',
-    height: 50,
-    marginBottom: 15
   }
 })

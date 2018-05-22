63815641a62f564779ba3defa929fe67a6bd7fa3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-22 17:26:43 -0300

    Remove unused code from build script

--- scripts/android/bundle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PATH="node_modules/.bin:$PATH"
-react-native bundle \
-  --platform android \
-  --entry-file index.js \
-  --assets-dest android/app/src/main/res/
-  --bundle-output android/app/src/main/assets/index.android.bundle \
-  $@
--- scripts/ios/bundle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PATH="node_modules/.bin:$PATH"
-react-native bundle \
-  --platform ios \
-  --entry-file index.js \
-  --assets-dest ios \
-  --bundle-output ios/EmCasa/main.jsbundle \
-  $@

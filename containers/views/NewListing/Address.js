dc2d018de46fc384d1e782da0d8f128942baedb0
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-05-30 12:47:48 -0300

    Redirect unauthenticated users to login on new listing form

--- containers/views/NewListing/Address.js
+++ containers/views/NewListing/Address.js
@@ -1,9 +1,20 @@
 import {Component} from 'react'
+import {connect} from 'react-redux'
 
+import {getToken} from '@/redux/modules/auth/selectors'
 import Shell from '@/containers/shared/Shell'
 import Address from '@/components/newListing/Address'
 
+@connect((state) => ({jwt: getToken(state)}))
 export default class AddressFormScreen extends Component {
+  componentDidMount() {
+    const {jwt, navigation} = this.props
+    if (!jwt) {
+      navigation.navigate('listings')
+      navigation.navigate('auth')
+    }
+  }
+
   onSubmit = (value) => {
     const {navigation} = this.props
     navigation.navigate(

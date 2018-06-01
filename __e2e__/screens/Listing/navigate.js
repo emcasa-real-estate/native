d7733fd614b5cd4ee588695c257e1d40836e5c33
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-06-01 16:03:05 -0300

    Wait for app to load before listing tests

--- __e2e__/screens/Listing/navigate.js
+++ __e2e__/screens/Listing/navigate.js
@@ -2,7 +2,9 @@ import * as landing from '../Landing/selectors'
 import {map} from './selectors'
 
 export default async function navigateToListingScreen() {
-  await waitFor(element(landing.feed())).toBeVisible()
+  await waitFor(element(landing.nthCard(1)))
+    .toBeVisible()
+    .withTimeout(2000)
   await element(landing.nthCard(1)).tap()
   await waitFor(element(map()))
     .toExist()

f3e835ce141c74431585a325b0108e2ac7d2c5f3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-06-04 13:44:19 -0300

    Disable interest form button on inactive listing screen

--- containers/views/Listing/index.js
+++ containers/views/Listing/index.js
@@ -1,9 +1,15 @@
 import {Component} from 'react'
+import {connect} from 'react-redux'
 
+import {getData, isLoading} from '@/redux/modules/listings/data/selectors'
 import Shell, {Section, Footer} from '@/containers/shared/Shell'
 import Listing, {Price} from '@/containers/listings/Listing'
 import RelatedListings from '@/containers/listings/Feed/Related'
 
+@connect((state, {navigation}) => ({
+  data: getData(state, navigation.state.params) || {},
+  loading: isLoading(state, navigation.state.params)
+}))
 export default class ListingScreen extends Component {
   onInterest = () => {
     const {navigation} = this.props
@@ -11,7 +17,7 @@ export default class ListingScreen extends Component {
   }
 
   render() {
-    const {navigation} = this.props
+    const {navigation, data, loading} = this.props
     const {id} = navigation.state.params
 
     return (
@@ -19,17 +25,21 @@ export default class ListingScreen extends Component {
         scroll
         title={<Price nullable id={id} size={24} />}
         footer={
-          <Footer
-            color="green"
-            label="Marcar Visita"
-            onPress={this.onInterest}
-          />
+          !loading && data.isActive ? (
+            <Footer
+              color="green"
+              label="Marcar Visita"
+              onPress={this.onInterest}
+            />
+          ) : null
         }
       >
         <Listing id={id} />
-        <Section title="Veja Também">
-          <RelatedListings id={id} />
-        </Section>
+        {!loading && (
+          <Section title="Veja Também">
+            <RelatedListings id={id} />
+          </Section>
+        )}
       </Shell>
     )
   }

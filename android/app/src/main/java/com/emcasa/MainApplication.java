e91bb3c1897c2a28ffc90eb9f130a33cff2c33a9
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-06-05 19:47:03 -0300

    Camera and media library permissions

--- android/app/src/main/java/com/emcasa/MainApplication.java
+++ android/app/src/main/java/com/emcasa/MainApplication.java
@@ -35,7 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ImagePickerPackage(),
+          new ImagePickerPackage(),
           new SvgPackage(),
           new OrientationPackage(),
           new PhotoViewPackage(),

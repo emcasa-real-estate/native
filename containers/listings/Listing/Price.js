5edf254990255b653e0fa3d2faf0580135ed267a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-06-07 20:21:33 -0300

    Handle null data on listing screen header

--- containers/listings/Listing/Price.js
+++ containers/listings/Listing/Price.js
@@ -1,8 +1,8 @@
 import Price from '@/components/shared/Price'
 import {withListing} from './Loader'
 
-function ListingNavigationApp({data: {price}, ...props}) {
-  return <Price {...props}>{price}</Price>
+function ListingNavigationApp({data, ...props}) {
+  return data ? <Price {...props}>{data.price}</Price> : null
 }
 
 export default withListing(ListingNavigationApp)

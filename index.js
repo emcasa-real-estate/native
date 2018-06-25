ae163d02a03a62638eddcd03e7e375afad592508
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-06-25 19:14:36 -0300

    Remove react-navigation

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import initNavigation from './screens'
 
 // Temporary fix for
 // https://github.com/facebook/react-native/issues/17504
-// https://github.com/react-navigation/react-navigation/issues/3956
 YellowBox.ignoreWarnings([
   'Class RCTCxxModule was not exported',
   'Module RCTImageLoader requires main queue setup',
--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ module.exports = {
   preset: 'react-native',
   automock: false,
   unmockedModulePathPatterns: ['/node_modules/'],
-  transformIgnorePatterns: [
-    '/node_modules/(?!(jest-)?react-native|react-navigation)/'
-  ],
+  transformIgnorePatterns: ['/node_modules/(?!(jest-)?react-native)/'],
   modulePathIgnorePatterns: ['/redux/__mocks__', '/graphql/__mocks__']
 }

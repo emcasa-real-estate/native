ebb659c5d323b4409465a05f822bb693591a6c59
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-06-26 13:28:55 -0300

    Use keyboard spacer on android

--- src/components/layout/Shell.js
+++ src/components/layout/Shell.js
@@ -1,11 +1,11 @@
-import {View, Platform} from 'react-native'
+import {View} from 'react-native'
 import KeyboardSpacer from 'react-native-keyboard-spacer'
 
 export default function Shell({style, children, testID}) {
   return (
     <View style={[{flex: 1, display: 'flex'}].concat(style)} testID={testID}>
       {children}
-      {Platform.OS === 'ios' && <KeyboardSpacer />}
+      <KeyboardSpacer />
     </View>
   )
 }

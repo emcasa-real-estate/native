0062021be1f4e177d8eccc9609f257fbf9588a6d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-06-27 00:47:51 -0300

    Request cameraroll permissions

--- src/components/newListing/Gallery/ImagePicker/Options/index.js
+++ src/components/newListing/Gallery/ImagePicker/Options/index.js
@@ -1,24 +1,37 @@
 import {PureComponent} from 'react'
-import {View} from 'react-native'
+import {View, Platform} from 'react-native'
 
+import {withPermission} from '@/containers/Permission'
 import Camera from './Camera'
 import CameraRoll from './CameraRoll'
 import Button from './Button'
 import styles from './styles'
 
+@withPermission(
+  Platform.select({
+    android: 'readStorage',
+    ios: 'photo'
+  })
+)
 export default class ImagePickerOptions extends PureComponent {
   onPickImage = (images) => {
     this.props.onPickImage(images)
     this.props.onDismiss()
   }
 
+  componentDidMount() {
+    this.props.onRequestPermission()
+  }
+
   render() {
-    const {onDismiss} = this.props
+    const {onDismiss, permission} = this.props
     return (
       <View style={styles.container}>
         <View style={styles.body}>
           <Camera onPickImage={this.onPickImage} />
-          <CameraRoll onPickImage={this.onPickImage} />
+          {permission == 'authorized' && (
+            <CameraRoll onPickImage={this.onPickImage} />
+          )}
           <Button style={styles.closeButtonText} onPress={onDismiss}>
             Cancelar
           </Button>

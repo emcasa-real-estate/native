bdab45e99f34a573b655e975136bba05b869ee6d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-06-27 12:51:00 -0300

    Disable gallery when listing has mo images

--- src/components/listings/Listing/Thumbnail/index.js
+++ src/components/listings/Listing/Thumbnail/index.js
@@ -76,9 +76,11 @@ export default class ListingThumbnail extends Component {
             )}
           </View>
           <View style={styles.actionCell}>
-            <ActionButton title="Ver imagens" onPress={this.onOpenGallery}>
-              <Icon name="image" />
-            </ActionButton>
+            {images.length && (
+              <ActionButton title="Ver imagens" onPress={this.onOpenGallery}>
+                <Icon name="image" />
+              </ActionButton>
+            )}
             {matterportCode && (
               <ActionButton
                 title="Ver em tela cheia"

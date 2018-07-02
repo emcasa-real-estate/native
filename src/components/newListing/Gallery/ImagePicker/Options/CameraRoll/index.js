1acf5c17fc2a8737587f4130ec47004fe8919bf8
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-02 18:34:01 -0300

    Fix image upload with cameraroll on android

--- src/components/newListing/Gallery/ImagePicker/Options/CameraRoll/index.js
+++ src/components/newListing/Gallery/ImagePicker/Options/CameraRoll/index.js
@@ -6,10 +6,12 @@ import Icon from '@/components/shared/Icon'
 import ListPicker from './Picker'
 import styles from './styles'
 
+const ext = (fileType) => fileType.split('/')[1]
+
 const getImage = ({node}) => ({
   type: node.type,
   uri: node.image.uri,
-  fileName: node.image.filename,
+  fileName: node.image.filename || `image.${ext(node.type)}`,
   width: node.image.width,
   height: node.image.height
 })
@@ -41,6 +43,7 @@ export default class CameraRollPicker extends PureComponent {
       first: 15,
       after: this.state.endCursor
     })
+    console.log(edges)
     this.setState(({images}) => ({
       images: images.concat(edges.map(getImage)),
       hasNextPage: page_info.has_next_page,

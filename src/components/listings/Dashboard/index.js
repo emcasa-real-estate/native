3c727f0e039ac61fcfe226d954a193e5ba3475e8
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-03 16:33:29 -0300

    Add more info to the dashboard

--- src/components/listings/Dashboard/index.js
+++ src/components/listings/Dashboard/index.js
@@ -3,13 +3,13 @@ import {View} from 'react-native'
 import * as format from '@/assets/format'
 import Text from '@/components/shared/Text'
 import Icon from '@/components/shared/Icon'
-import styles from './styles'
+import styles, {iconColor} from './styles'
 
 function Property({title, icon, value}) {
   return (
     <View style={styles.row}>
       <View style={styles.propTitle}>
-        <Icon name={icon} />
+        <Icon name={icon} color={iconColor} size={22} />
         <Text style={styles.propTitleText}>{title}</Text>
       </View>
       <View style={styles.propValue}>
@@ -21,27 +21,33 @@ function Property({title, icon, value}) {
 
 export default function ListingDashboard({
   insertedAt,
-  interestCount,
   inPersonVisitCount,
   visualisations,
   tourVisualisations,
+  favoriteCount,
   matterportCode
 }) {
   return (
     <View style={styles.container}>
       <Property
         title="Data de criação"
-        icon="calendar-alt"
+        icon="calendar-check"
         value={format.date(new Date(insertedAt))}
       />
       <Property title="Visualizações" icon="eye" value={visualisations} />
       {matterportCode && (
         <Property
           title="Visualizações do Tour 3D"
-          icon="eye"
+          icon="home-heart"
           value={tourVisualisations}
         />
       )}
+      <Property title="Favoritos" icon="heart" value={favoriteCount} />
+      <Property
+        title="Visitas marcadas"
+        icon="calendar-alt"
+        value={inPersonVisitCount}
+      />
     </View>
   )
 }
--- src/components/listings/Dashboard/styles.js
+++ src/components/listings/Dashboard/styles.js
@@ -5,16 +5,32 @@ import * as colors from '@/assets/colors'
 export default StyleSheet.create({
   container: {
     flex: 1,
-    display: 'flex'
+    display: 'flex',
+    justifyContent: 'flex-start'
   },
   row: {
-    flex: 1,
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.gray.lighter
   },
   propTitle: {
     display: 'flex',
     flexDirection: 'row'
+  },
+  propTitleText: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginLeft: 10,
+    color: colors.gray.dark
+  },
+  propValueText: {
+    fontSize: 16,
+    color: colors.gray.dark
   }
 })
+
+export const iconColor = colors.gray.dark

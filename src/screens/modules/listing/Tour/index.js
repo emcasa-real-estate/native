925b88bbf6162f555600ddcd1b77610074cda1e2
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-05 02:46:56 -0300

    Graphql listing query

--- src/screens/modules/listing/Tour/index.js
+++ src/screens/modules/listing/Tour/index.js
@@ -1,19 +1,12 @@
 import {PureComponent} from 'react'
 import {connect} from 'react-redux'
 
-import {getData} from '@/redux/modules/listings/data/selectors'
+import composeWithRef from '@/lib/composeWithRef'
+import {withListing} from '@/graphql/containers'
 import {Modal, Body} from '@/components/layout'
 import Matterport from '@/components/listings/Matterport'
 
-@connect(
-  (state, {params}) => ({
-    data: getData(state, params) || {}
-  }),
-  null,
-  null,
-  {withRef: true}
-)
-export default class ListingTourScreen extends PureComponent {
+class ListingTourScreen extends PureComponent {
   static screenName = 'listing.Tour'
 
   static options = {
@@ -27,20 +20,24 @@ export default class ListingTourScreen extends PureComponent {
   onLayout = ({nativeEvent: {layout}}) => this.setState({layout})
 
   render() {
-    const {data, onDismiss} = this.props
+    const {listing: {data, loading}, onDismiss} = this.props
     const {layout} = this.state
 
     return (
       <Modal testID="@listing.Tour">
-        <Body onLayout={this.onLayout}>
+        <Body loading={loading} onLayout={this.onLayout}>
           <Modal.Header
             iconColor="white"
             style={{marginTop: 60, justifyContent: 'flex-end'}}
             onDismiss={onDismiss}
           />
-          <Matterport code={data.matterportCode} {...layout} />
+          {data && <Matterport code={data.matterportCode} {...layout} />}
         </Body>
       </Modal>
     )
   }
 }
+
+export default composeWithRef(withListing(({params: {id}}) => ({id})))(
+  ListingTourScreen
+)

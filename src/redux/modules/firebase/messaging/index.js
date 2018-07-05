c054c2d29ec1609143f34c22c590d7d6be784be5
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-05 12:54:47 -0300

    Listen to notifications

--- src/redux/modules/firebase/messaging/index.js
+++ src/redux/modules/firebase/messaging/index.js
@@ -1,6 +1,13 @@
 export const UPDATE_TOKEN = 'firebase/messaging/UPDATE_TOKEN'
 export const UPDATE_PERMISSION = 'firebase/messaging/UPDATE_PERMISSION'
 export const REQUEST_PERMISSION = 'firebase/messaging/REQUEST_PERMISSION'
+// Notification received while app is opened
+export const NOTIFICATION_RECEIVED = 'firebase/messaging/NOTIFICATION_RECEIVED'
+// Notification opened while app is in background
+export const NOTIFICATION_OPENED = 'firebase/messaging/NOTIFICATION_OPENED'
+// App was opened by a notification
+export const NOTIFICATION_INITIALIZED =
+  'firebase/messaging/NOTIFICATION_INITIALIZED'
 
 export const updateToken = (token) => ({type: UPDATE_TOKEN, token})
 export const requestPermission = () => ({type: REQUEST_PERMISSION})
@@ -8,10 +15,25 @@ export const updatePermission = (granted) => ({
   type: UPDATE_PERMISSION,
   granted
 })
+export const notificationReceived = (notification) => ({
+  type: NOTIFICATION_RECEIVED,
+  notification
+})
+export const notificationOpened = (notification, action) => ({
+  type: NOTIFICATION_OPENED,
+  notification,
+  action
+})
+export const notificationInitialized = (notification, action) => ({
+  type: NOTIFICATION_INITIALIZED,
+  notification,
+  action
+})
 
 export const initialState = {
   hasPermission: undefined,
-  token: undefined
+  token: undefined,
+  initialNotification: undefined
 }
 
 export default function fcmReducer(state = initialState, action) {
@@ -20,6 +42,14 @@ export default function fcmReducer(state = initialState, action) {
       return {...state, token: action.token}
     case UPDATE_PERMISSION:
       return {...state, hasPermission: action.granted}
+    case NOTIFICATION_INITIALIZED:
+      return {
+        ...state,
+        initialNotification: {
+          notification: action.notification,
+          action: action.action
+        }
+      }
     default:
       return state
   }
--- src/redux/modules/firebase/messaging/saga.js
+++ src/redux/modules/firebase/messaging/saga.js
@@ -1,27 +1,41 @@
-import {eventChannel, END} from 'redux-saga'
+import {eventChannel} from 'redux-saga'
 import {
   all,
   call,
   put,
   fork,
   take,
-  select,
-  takeLatest
+  takeLatest,
+  takeEvery
 } from 'redux-saga/effects'
 import Firebase from 'react-native-firebase'
 
-import {getUser} from '@/redux/modules/auth/selectors'
-import * as auth from '@/redux/modules/auth'
 import * as actions from './index'
 
 const messaging = Firebase.messaging()
+
+const notifications = Firebase.notifications()
 
 const getToken = () => messaging.getToken()
 
 const hasPermission = () => messaging.hasPermission()
 
+const getInitialNotification = () => notifications.getInitialNotification()
+
 const tokenRefreshChannel = () =>
   eventChannel((emit) => messaging.onTokenRefresh((token) => emit({token})))
+
+const foregroundNotificationChannel = () =>
+  eventChannel((emit) =>
+    notifications.onNotification((notification) => emit({notification}))
+  )
+
+const backgroundNotificationChannel = () =>
+  eventChannel((emit) =>
+    notifications.onNotificationOpened(({notification, action}) =>
+      emit({notification, action})
+    )
+  )
 
 function* requestPermission() {
   try {
@@ -30,6 +44,14 @@ function* requestPermission() {
   } catch (err) {
     yield put(actions.updatePermission(false))
   }
+}
+
+function* notificationReceived({notification}) {
+  yield put(actions.notificationReceived(notification))
+}
+
+function* notificationOpened({notification, action}) {
+  yield put(actions.notificationOpened(notification, action))
 }
 
 function* initializeToken() {
@@ -44,10 +66,21 @@ function* initializePermission() {
   yield put(actions.updatePermission(enabled))
 }
 
+function* initializeNotification() {
+  const response = yield call(getInitialNotification)
+  if (response)
+    yield put(
+      actions.notificationInitialized(response.notification, response.action)
+    )
+}
+
 export default function* fcmSaga() {
   yield all([
     takeLatest(actions.REQUEST_PERMISSION, requestPermission),
+    takeEvery(backgroundNotificationChannel(), notificationOpened),
+    takeEvery(foregroundNotificationChannel(), notificationReceived),
+    fork(initializePermission),
     fork(initializeToken),
-    fork(initializePermission)
+    fork(initializeNotification)
   ])
 }

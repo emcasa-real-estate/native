fdfa66f935fbe6d95b743d3ceea14d6ef19a0bf3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-06 10:24:24 -0300

    Animate gallery pagination

--- src/components/listings/Gallery/Pagination/index.js
+++ src/components/listings/Gallery/Pagination/index.js
@@ -1,43 +1,88 @@
 import _ from 'lodash'
 import {PureComponent} from 'react'
-import {View} from 'react-native'
+import {View, Animated} from 'react-native'
 
 import Icon from '@/components/shared/Icon'
 import styles from './styles'
 
 export default class GalleryPagination extends PureComponent {
-  renderIcon = (index) => {
-    let {currentPosition, totalPages} = this.props
-    const limit = 2
-    let right = currentPosition + limit
-    let left = currentPosition - limit
-    if (left < 0) right -= left
-    if (right >= totalPages) left = totalPages - limit * 2 - 1
-    if (index < left || index > right) return null
-    const distanceFromActivePage = Math.min(
-      limit,
-      Math.abs(index - currentPosition)
+  static defaultProps = {
+    maxLength: 11
+  }
+
+  state = {
+    length: undefined,
+    scale: []
+  }
+
+  get cappedPosition() {
+    const {currentPosition, totalPages} = this.props
+    const {length} = this.state
+    const {max, min, floor} = Math
+    return max(
+      length - (totalPages - currentPosition),
+      min(currentPosition, floor(length / 2))
     )
-    const size = 12 / (distanceFromActivePage + 1) + 2
-    const opacity = 0.6 / (distanceFromActivePage + 1) + 0.4
+  }
+
+  calculateLayout() {
+    const {length} = this.state
+    const position = this.cappedPosition
+    let layout = []
+    for (let i = 0; i < length; ++i)
+      layout[i] = 1 / (Math.abs(position - i) + 1)
+    return layout
+  }
+
+  updateLayout(animate = true) {
+    this.calculateLayout().forEach((scale, index) => {
+      const value = this.state.scale[index]
+      if (value._value === scale) return
+      value.stopAnimation()
+      if (!animate) value.setValue(scale)
+      else
+        Animated.timing(value, {
+          toValue: scale,
+          duration: 200,
+          useNativeDriver: true
+        }).start()
+    })
+  }
+
+  static getDerivedStateFromProps({totalPages, maxLength}, state) {
+    const nextState = {...state, length: Math.min(totalPages, maxLength)}
+    if (state.length !== nextState.length)
+      for (let i = 0; i < nextState.length; ++i)
+        nextState.scale[i] = new Animated.Value(0)
+    return nextState
+  }
+
+  componentDidMount() {
+    this.updateLayout(false)
+  }
+
+  componentDidUpdate(prev) {
+    if (prev.currentPosition !== this.props.currentPosition) this.updateLayout()
+  }
+
+  renderIcon = (index) => {
+    const scale = this.state.scale[index]
+    const value = scale.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0.3, 1]
+    })
+    const style = {opacity: value, transform: [{scale: value}]}
     return (
-      <Icon
-        key={index}
-        type="solid"
-        name="circle"
-        color="white"
-        size={size}
-        style={[styles.pageIcon, {opacity}]}
-      />
+      <Animated.View key={index} style={[styles.pageIcon, style]}>
+        <Icon type="solid" name="circle" color="white" size={10} />
+      </Animated.View>
     )
   }
 
   render() {
-    const {totalPages} = this.props
+    const {length} = this.state
     return (
-      <View style={styles.container}>
-        {_.times(totalPages, this.renderIcon)}
-      </View>
+      <View style={styles.container}>{_.times(length, this.renderIcon)}</View>
     )
   }
 }

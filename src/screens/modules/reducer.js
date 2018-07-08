287ff34a6c7f29c990377dc69ba75ae094796825
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-08 18:18:00 -0300

    Remove listing form reducer

--- src/screens/modules/reducer.js
+++ src/screens/modules/reducer.js
@@ -3,11 +3,9 @@ import {combineReducers} from 'redux'
 import context from './context'
 import navigation from './navigation'
 import listings from './listings/reducer'
-import listingForm from './listingForm/reducer'
 
 export default combineReducers({
   context,
   navigation,
-  listings,
-  listingForm
+  listings
 })

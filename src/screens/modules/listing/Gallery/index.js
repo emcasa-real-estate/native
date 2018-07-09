cbfb6bd88b008477805ae0d9cc0dcbd2444eb5b2
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-09 20:05:07 -0300

    Fix gallery screen initial state

--- src/screens/modules/listing/Gallery/index.js
+++ src/screens/modules/listing/Gallery/index.js
@@ -29,7 +29,7 @@ export default class ListingGalleryScreen extends PureComponent {
       <Modal testID="@listing.Gallery">
         <Body>
           <Modal.Header iconColor="white" onDismiss={onDismiss} />
-          <Gallery scalable>{data.images}</Gallery>
+          {data && <Gallery>{data.images}</Gallery>}
         </Body>
       </Modal>
     )

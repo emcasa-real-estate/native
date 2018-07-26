bea4803b5fa2cc5f6f70bfb261cea19f3a1050d1
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-26 18:22:10 -0300

    Update e2e tests

--- __e2e__/screens/listingForm/screen.spec.js
+++ __e2e__/screens/listingForm/screen.spec.js
@@ -8,7 +8,10 @@ describe('listingForm', () => {
   context('unauthenticated', () => {
     beforeAll(actions.navigate)
 
-    it('redirects to the login page', async () => {
+    it('redirects to a modal within the login page', async () => {
+      await expect(element(select.learnMoreScreen())).toBeVisible()
+      await element(by.id('close_modal_button')).tap()
+      await waitFor(element(select.learnMoreScreen())).toBeNotVisible()
       await expect(element(loginScreen())).toBeVisible()
       await element(backButton()).tap()
     })
@@ -20,13 +23,6 @@ describe('listingForm', () => {
       await navigateToLogin()
       await login({email: 'test@example.com', password: 'passwd'})
       await actions.navigate()
-    })
-
-    it('opens a modal on mount', async () => {
-      await waitFor(element(select.addressScreen())).toBeNotVisible()
-      await expect(element(select.learnMoreScreen())).toBeVisible()
-      await element(by.id('close_modal_button')).tap()
-      await waitFor(element(select.addressScreen())).toBeVisible()
     })
 
     require('./Address')

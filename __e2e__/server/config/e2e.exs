1e941e006d3e20b25bd53662c20b4d732f7c6f79
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-07-27 19:48:07 -0300

    Setup backend configuration and seed data

---
+++ __e2e__/server/config/e2e.exs
@@ -0,0 +1,34 @@
+use Mix.Config
+
+import_config "e2e.secret.exs"
+
+# For development, we disable any cache and enable
+# debugging and code reloading.
+#
+# The watchers configuration can be used to run external
+# watchers to your application. For example, we use it
+# with brunch.io to recompile .js and .css sources.
+config :re, ReWeb.Endpoint,
+  http: [port: System.get_env("BACKEND_PORT") || 4000],
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  watchers: []
+
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "[$level] $message\n"
+
+# Set a higher stacktrace during development. Avoid configuring such
+# in production as building large stacktraces may be expensive.
+config :phoenix, :stacktrace_depth, 20
+
+# Configure your database
+config :re, Re.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "re_test",
+  hostname: "localhost",
+  pool_size: 10
+
+config :re, ReWeb.Notifications.Emails.Mailer, adapter: Swoosh.Adapters.Local
--- __e2e__/setup.framework.js
+++ __e2e__/setup.framework.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-console */
 import path from 'path'
 import detox from 'detox'
-import {exec} from 'child_process'
+import {promisify} from 'util'
+import * as child_process from 'child_process'
 
 import pkg from '../package.json'
 
+const exec = promisify(child_process.exec)
+
 const DEVICE_NAME = process.env.DEVICE_NAME || 'booted'
+const DB_NAME = process.env.CLEANUP_DB_NAME
 const SCREENSHOT_PATH =
   process.env.SCREENSHOT_PATH || path.join(__dirname, '../tmp/screenshots')
 
@@ -17,24 +22,33 @@ const timestamp = () => {
     .replace('@t', date.getTime())
 }
 
-global.screenShot = (fileName = timestamp()) =>
-  new Promise((resolve) =>
-    exec(
-      `xcrun simctl io "${DEVICE_NAME}" screenshot ${SCREENSHOT_PATH}/${fileName}.png`,
-      (error) => {
-        /* eslint-disable no-console */
-        if (error) console.error(error.message)
-        else
-          console.info(`Saved screenshot to ${SCREENSHOT_PATH}/${fileName}.png`)
-        resolve()
-        /* eslint-enable */
-      }
+global.screenShot = async (fileName = timestamp()) => {
+  try {
+    await exec(
+      `xcrun simctl io "${DEVICE_NAME}" screenshot ${SCREENSHOT_PATH}/${fileName}.png`
     )
-  )
+    console.info(`Saved screenshot to ${SCREENSHOT_PATH}/${fileName}.png`)
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+async function resetDatabase() {
+  if (!DB_NAME) return
+  const dumpFile = `${__dirname}/server/re_test.dump`
+  try {
+    await exec(`psql ${DB_NAME} < ${dumpFile}`)
+    console.log(`Database ${DB_NAME} has been reset.`)
+  } catch (error) {
+    console.error(error.message)
+  }
+}
 
 jest.setTimeout(180000)
 
 beforeAll(() => detox.init(pkg.detox))
+
+beforeAll(() => resetDatabase())
 
 afterEach(() => screenShot())
 

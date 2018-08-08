3568e719e9ca396941bffe2339b14a31e37ef276
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-08-08 16:00:35 -0300

    Shell "none" keyboard behavior

--- src/components/layout/Shell.js
+++ src/components/layout/Shell.js
@@ -3,29 +3,34 @@ import {View, KeyboardAvoidingView, Dimensions} from 'react-native'
 
 export default class Shell extends PureComponent {
   static defaultProps = {
-    avoidKeyboard: true
+    behavior: 'padding'
   }
 
   state = {}
 
   onLayout = ({nativeEvent: {layout}}) =>
     this.setState({
-      offset: Math.max(0, Dimensions.get('window').height - layout.height)
+      offset: Math.max(0, Dimensions.get('window').height - layout.height),
+      layout: {height: layout.height}
     })
 
   render() {
-    const {style, children, testID, avoidKeyboard} = this.props
-    const {offset} = this.state
-    const ViewComponent = avoidKeyboard ? KeyboardAvoidingView : View
+    const {style, children, testID, behavior} = this.props
+    const {offset, layout} = this.state
     return (
-      <View testID={testID} style={{flex: 1}} onLayout={this.onLayout}>
-        <ViewComponent
+      <View
+        testID={testID}
+        style={[{flex: 1}, layout]}
+        onLayout={this.onLayout}
+      >
+        <KeyboardAvoidingView
           style={[{flex: 1}, style]}
           keyboardVerticalOffset={offset}
-          behavior="padding"
+          behavior={behavior !== 'none' ? behavior : undefined}
+          enabled={behavior !== 'none'}
         >
           <View style={{flex: 1, display: 'flex'}}>{children}</View>
-        </ViewComponent>
+        </KeyboardAvoidingView>
       </View>
     )
   }

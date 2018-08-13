f6266a9e48ad7093d664c71c280ddffaecb80120
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-08-13 13:04:13 -0300

    Update login/sign up tests

--- __e2e__/screens/Auth/Login.spec.js
+++ __e2e__/screens/Auth/Login.spec.js
@@ -11,11 +11,6 @@ describe('auth.Login', () => {
     await expect(element(by.text('A senha é obrigatória'))).toBeVisible()
   })
 
-  it('shows an error message on failure', async () => {
-    await action.login({email: 'foo@example.com', password: 'wrong_password'})
-    await expect(element(by.text('Senha ou email inválidos'))).toBeVisible()
-  })
-
   it('redirects to the main screen on success', async () => {
     await action.login({email: 'foo@example.com', password: 'password'})
     await waitFor(element(select.loginScreen()))
--- __e2e__/server/api/index.js
+++ __e2e__/server/api/index.js
@@ -7,10 +7,7 @@ const MOCKS_DIR = path.join(__dirname, 'mocks')
 const read = promisify(fs.readFile)
 
 export default async function restMiddleware(req, res, next) {
-  const fileName = path.join(
-    MOCKS_DIR,
-    `${req.path}/${req.method.toLowerCase()}.json`
-  )
+  const fileName = path.join(MOCKS_DIR, `${req.path}.json`)
   if (fs.existsSync(fileName)) {
     const data = await read(fileName)
     res.status(200).json(JSON.parse(data))

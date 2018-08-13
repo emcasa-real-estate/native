4a58cacebcf09ac7ecfacd966c3b7bfcd17e8950
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-08-13 13:26:40 -0300

    Remove introspection schema

--- __e2e__/server/graphql/index.js
+++ __e2e__/server/graphql/index.js
@@ -1,12 +1,18 @@
+import path from 'path'
+import fs from 'fs'
 import {ApolloServer} from 'apollo-server-express'
-import {buildClientSchema} from 'graphql'
-import {addMockFunctionsToSchema} from 'graphql-tools'
+import {makeExecutableSchema, addMockFunctionsToSchema} from 'graphql-tools'
 
 import mocks from './mocks'
 
 export async function createSchema() {
+  const typeDefs = fs
+    .readFileSync(path.join(__dirname, 'schema.graphql'))
+    .toString()
   // Make a GraphQL schema with no resolvers
-  const schema = buildClientSchema(require('./schema.json'))
+  const schema = makeExecutableSchema({
+    typeDefs
+  })
   // Add mocks, modifies schema in place
   addMockFunctionsToSchema({schema, mocks})
   return schema

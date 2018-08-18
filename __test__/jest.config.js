3b7f3cfe185aec25702b5e1c6e6955dcf6042b4e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-08-18 15:31:44 -0300

    follow up

--- __test__/jest.config.js
+++ __test__/jest.config.js
@@ -2,6 +2,9 @@ const config = require('../jest.config.js')
 
 module.exports = {
   ...config,
+  transform: {
+    '^.+\\.js$': 'react-native/jest/preprocessor.js'
+  },
   testMatch: [`${__dirname}/**/*.spec.js`],
   setupFiles: ['./__test__/setup.js', 'jest-plugin-context/setup'],
   setupTestFrameworkScriptFile: './__test__/setup.framework.js'
--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   rootDir: __dirname,
   preset: 'react-native',
   automock: false,
-  transform: {
-    '^.+\\.js$': 'react-native/jest/preprocessor.js'
-  },
   unmockedModulePathPatterns: ['/node_modules/'],
   transformIgnorePatterns: ['/node_modules/(?!(jest-)?react-native)/'],
   modulePathIgnorePatterns: ['/src/redux/__mocks__', '/src/graphql/__mocks__']

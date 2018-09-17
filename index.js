6fca1984b69f87511a9077f37a70bff6382b286a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-09-17 18:47:35 -0300

    Disable ak advanced ui

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import {Component} from 'react'
-import {View, Text, Button} from 'react-native'
 import 'moment/locale/pt-br'
 import moment from 'moment'
 import {YellowBox} from 'react-native'
@@ -7,31 +5,10 @@ import AccountKit from 'react-native-facebook-account-kit'
 
 import initNavigation from '@/screens'
 
-const akView = (text) =>
-  class MyComponent extends Component {
-    static options = {
-      width: 100,
-      height: 50
-    }
-
-    render() {
-      return (
-        <View style={{flex: 1, backgroundColor: 'gray', height: 50}}>
-          <Button onPress={() => console.log('pressed', text)} title={text} />
-        </View>
-      )
-    }
-  }
-
 AccountKit.configure({
   responseType: 'token',
   defaultCountry: 'BR',
-  receiveSMS: true,
-  advancedUI: {
-    header: akView('Header'),
-    footer: akView('Footer'),
-    body: akView('Body')
-  }
+  receiveSMS: true
 })
 
 // Temporary fix for

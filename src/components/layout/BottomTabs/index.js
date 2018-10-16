ceb681bfc2418d26f7597807e036392a1c90d249
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-10-16 08:09:49 -0300

    BottomTabs container

--- src/components/layout/BottomTabs/index.js
+++ src/components/layout/BottomTabs/index.js
@@ -1,22 +1,47 @@
 import {View} from 'react-native'
 import styled from 'styled-components/native'
+import {connect} from 'react-redux'
 
+import {switchTab} from '@/screens/modules/navigation'
+import {getCurrentTabIndex} from '@/screens/modules/navigation/selectors'
+import {withJwt} from '@/graphql/containers/CredentialsQuery'
 import Button from './Button'
 import Option from './Option'
+import {compose} from 'recompose'
 
-const BottomTabs = styled(({jwt, children, onChange, ...props}) => (
+const BottomTabs = styled(({jwt, children, tabIndex, onChange, ...props}) => (
   <View testID="bottom_tabs" {...props}>
-    <Option icon="search" type="solid" onPress={() => onChange(0)}>
+    <Option
+      icon="search"
+      type="solid"
+      active={tabIndex == 0}
+      onPress={() => onChange(0)}
+    >
       Explorar
     </Option>
-    <Option icon="flag" type="solid" onPress={() => onChange(1)}>
+    <Option
+      icon="flag"
+      type="solid"
+      active={tabIndex == 1}
+      onPress={() => onChange(1)}
+    >
       Anunciar
     </Option>
     {children}
-    <Option icon="heart" type="solid" onPress={() => onChange(2)}>
+    <Option
+      icon="heart"
+      type="solid"
+      active={tabIndex == 2}
+      onPress={() => onChange(2)}
+    >
       Favoritos
     </Option>
-    <Option icon="user" type="solid" onPress={() => onChange(3)}>
+    <Option
+      icon="user"
+      type="solid"
+      active={tabIndex == 3}
+      onPress={() => onChange(3)}
+    >
       {jwt ? 'Meu Perfil' : 'Login'}
     </Option>
   </View>
@@ -26,6 +51,14 @@ const BottomTabs = styled(({jwt, children, onChange, ...props}) => (
   justify-content: space-around;
 `
 
-BottomTabs.Button = Button
+const BottomTabsContainer = compose(
+  withJwt,
+  connect(
+    (state) => ({tabIndex: getCurrentTabIndex(state)}),
+    {onChange: switchTab}
+  )
+)(BottomTabs)
 
-export default BottomTabs
+BottomTabsContainer.Button = Button
+
+export default BottomTabsContainer

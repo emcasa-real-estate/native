5da79178b05a7332f5b27e30c5e8e6b434d62ca2
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-10-23 14:53:04 -0200

    Remove map screen

--- src/screens/modules/listings/screens.js
+++ src/screens/modules/listings/screens.js
@@ -1,4 +1,2 @@
 export {default as Feed} from './Feed'
-export {default as Map} from './Map'
-export {LocationHeaderButton, FilterHeaderButton} from './Map/HeaderButton'
 export {default as Search} from './Search'
--- src/screens/modules/reducer.js
+++ src/screens/modules/reducer.js
@@ -2,10 +2,8 @@ import {combineReducers} from 'redux'
 
 import context from './context'
 import navigation from './navigation'
-import listings from './listings/reducer'
 
 export default combineReducers({
   context,
-  navigation,
-  listings
+  navigation
 })
